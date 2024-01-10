55e6abb2e3d93e3e4cef5647b17ffb83a5cbe3b3
cristian <cristian0402218@gmail.com>
2024-01-10 14:41:15 -0600

    :building_construction: Add all components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import BackTopButton from './components/BackTopButton'
+import Documentation from './components/Documentation'
+import Faqs from './components/Faqs'
+import Home from './components/Home'
+import Nav from './components/Nav'
+import News from './components/News'
+import SearchCoins from './components/SearchCoins'
+import TrendingCryptos from './components/TrendingCryptos'
+
 function App() {
   return (
-    <section>
-      <h1>CoinTegu</h1>
-    </section>
+    <>
+      <Nav />
+      <div className="mx-auto flex max-w-[1600px] flex-col justify-center gap-8 bg-[#fffffe] px-6 pb-8 yellow-light dark:bg-[#000] dark:yellow-dark md:px-12 2xl:px-0">
+        <Home />
+        <News />
+        <TrendingCryptos />
+        <SearchCoins />
+        <Faqs />
+        <Documentation />
+        <BackTopButton />
+      </div>
+    </>
   )
 }
 
